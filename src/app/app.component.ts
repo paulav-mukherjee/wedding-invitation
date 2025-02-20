9b0d1c97a26da52222937d73d60ceb081e5291d3
Paulav <paulavmukherjee@gmail.com>
2025-02-21 01:47:26 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,35 +7,14 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: [ 'app.component.scss' ],
 })
 export class AppComponent {
-  name: any = ''
-  wedding_from : any = ''
+
   constructor(
     private route: ActivatedRoute,
     private router: Router,
   ) {
     this.route.queryParams.subscribe((params: any) => {
 
-      if (params.type) {
-        this.wedding_from = (params.type).toLowerCase()
-         localStorage.setItem('invitation_from', (params.type).toLowerCase()) // wedding or reception
-      } else {
-        const weddingType = localStorage.getItem('invitation_from');
-        if (weddingType) {
-          this.wedding_from = weddingType;
-        }
-      }
-      if (params.name) {
-        this.name = params.name;
-        localStorage.setItem('invitation_name', params.name);
-        this.router.navigate([ 'home' ], { queryParams: {} });
-      }
 
-      else {
-        const storedName = localStorage.getItem('invitation_name');
-        if (storedName) {
-          this.name = storedName;
-        }
-      }
     });
   }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -16,7 +16,9 @@ export class HomePage implements OnInit, DoCheck, AfterViewInit {
   wedding_type = '';
   e_card_link: any = '';
   contact: any = 'xxxxxxxxxx';
-
+  name: any = ''
+  wedding_from: any = ''
+  is_loading: boolean = true;
   targetDate: Date = new Date('2025-03-03T23:59:59'); // Set your target date here
   days: number = 0;
   hours: number = 0;
@@ -29,20 +31,54 @@ export class HomePage implements OnInit, DoCheck, AfterViewInit {
     private globals: Globals,
   ) {
     this.route.queryParams.subscribe((params: any) => {
-      this.employee_id = params.employee_id
+      if (params.type) {
+        this.wedding_from = (params.type).toLowerCase()
+        localStorage.setItem('invitation_from', (params.type).toLowerCase()) // wedding or reception
+      } else {
+        const weddingType = localStorage.getItem('invitation_from');
+        if (weddingType) {
+          this.wedding_from = weddingType;
+        }
+      }
+      if (params.name) {
+        this.name = params.name;
+        localStorage.setItem('invitation_name', params.name);
+        this.router.navigate([ 'home' ], { queryParams: {} });
+      }
+
+      else {
+        const storedName = localStorage.getItem('invitation_name');
+        if (storedName) {
+          this.name = storedName;
+        }
+      }
     })
   };
 
   ngOnInit() { }
   ionViewDidEnter() {
     this.startCountdown();
-    $('#staticBackdrop').modal('show')
+    // $('#staticBackdrop').modal('show')
+
+    setTimeout(() => {
+      this.is_loading = false;
+    }, 4000);
+    setTimeout(() => {
+      document.getElementById("cuple2").scrollIntoView({
+        behavior: "smooth", // Smooth scrolling animation
+        block: "start",     // Scroll to the start of the element
+      });
+    }, 7000);
+    setTimeout(() => {
+      document.getElementById("coupl3").scrollIntoView({
+        behavior: "smooth", // Smooth scrolling animation
+        block: "start",     // Scroll to the start of the element
+      });
+    }, 10000);
   }
 
   ngDoCheck(): void {
     this.wedding_type = localStorage.getItem('invitation_from') ? localStorage.getItem('invitation_from') : '';
-    console.log("🚀 ~ HomePage ~ ngDoCheck ~ this.wedding_type:", this.wedding_type)
-
     if (this.wedding_type == 'wedding') {
       this.e_card_link = 'assets/img/section1/paulav_and_souravi_wedding_invitation.pdf'
       this.contact = '7003055308'
@@ -54,18 +90,20 @@ export class HomePage implements OnInit, DoCheck, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    const audioElement: HTMLAudioElement | null = document.getElementById('bgMusic') as HTMLAudioElement;
-    audioElement.volume = 0.3;
-    audioElement.play();
-    this.music_status = 'play';
+    setTimeout(() => {
+      const audioElement: HTMLAudioElement | null = document.getElementById('bgMusic') as HTMLAudioElement;
+      audioElement!.volume = 0.3;
+      audioElement!.play();
+      this.music_status = 'play';
+    }, 4000);
   }
 
-  isTimeLeft : boolean = true;
-  startCountdown(): void { 
+  isTimeLeft: boolean = true;
+  startCountdown(): void {
     const updateTime = () => {
       const now = new Date().getTime();
       const timeLeft = this.targetDate.getTime() - now;
-  
+
       if (timeLeft >= 0) {
         this.isTimeLeft = true;
         // Countdown logic
@@ -83,7 +121,7 @@ export class HomePage implements OnInit, DoCheck, AfterViewInit {
         this.seconds = Math.floor((timeExceeded % (1000 * 60)) / 1000);
       }
     };
-  
+
     updateTime(); // Initialize countdown
     const intervalId = setInterval(updateTime, 1000);
   }
@@ -104,6 +142,14 @@ export class HomePage implements OnInit, DoCheck, AfterViewInit {
         audioElement.play();
         this.music_status = 'play';
       }
+    }
+  }
+
+  gotoMap(type: any) {
+    if (type == 'mpj') {
+      window.open('https://maps.app.goo.gl/tF4UNQSiY7JtrVgL7', '_blank')
+    } else if (type == 'hlr') {
+      window.open('https://maps.app.goo.gl/QwJF41BH3FTwtjNK8', '_blank')
     }
   }
 }
